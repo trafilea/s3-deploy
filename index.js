7d0eca36c139269eff36f34a8a02cac09cc15475
CristianArboleda <cristianm.arboleda@gmail.com>
2025-03-04 19:17:56 -0500

    fix cache control parameter name

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function run() {
     const private = getBooleanInput('private');
     const immutable = getBooleanInput('immutable');
 
-    const cacheControl = core.getInput('cacheControl');
+    const cacheControl = core.getInput('cache-control');
     const cache = core.getInput('cache') || null;
     const filesToInclude = core.getInput('files-to-include') || null;
 
